f795d300542068c0f354356525d005a54338e88a
Olha Tarasova <tarasovao0505@gmail.com>
2021-08-11 12:07:36 +0300

    Добавлен слушатель

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,22 @@
-const fetchCountries = fetch('https://restcountries.eu/rest/v2/name/eesti')
-  .then(response => {
-    return response.json();
-  })
-  .then(country => {
-    console.log(country);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+import countriesCardTpl from '../templates/countries-card.hbs';
+import debounce from 'lodash.debounce';
+import API from './api-service';
+import getRefs from './get-refs';
+
+const refs = getRefs();
+
+function onSearch(evt) {
+  searchCountries.innerHTML = '';
+  const searchQuery = evt.target.value;
+}
+
+API.fetchCountry(searchQuery)
+  .then(renderCountryCard)
+  .catch(error => console.log(error));
+
+function renderCountryCard(country) {
+  const markup = countriesCardTpl(country);
+  refs.cardCountries.innerHTML = markup;
+}
+
+input.addEventListener('input', debounce(onSearch, 500));
---
+++ src/js/get-refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+  return {
+    cardCountries: document.querySelector('.js-card-countries'),
+    searchCountries: document.querySelector('#search-country'),
+  };
+}
